9acfeb23020f972998ffd8570ee0dd641f9fb867
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2011-08-05 06:25:52 +0900

    Fixed suffix array from char* to size_t vector.

--- mysearch.cpp
+++ mysearch.cpp
@@ -14,30 +14,36 @@ extern "C"{
 class SuffixArray
 {
 private:
-  const char * reference;
+  const char * ref_str;
   const size_t ref_length;
-  std::vector<const char *> suffix_array;
+  std::vector<size_t> suffix_array;
   struct LessStr{
-    bool operator() (const char * a, const char * b) {
-      return std::strcmp(a, b) < 0;
+    const char * str;
+    LessStr(const char * str) : str(str) {}
+    bool operator() (size_t a, size_t b) {
+      return std::strcmp(str+a, str+b) < 0;
     }
   };
   class LessStrLimited{
+    const char * str;
     size_t limit;
   public:
-    LessStrLimited(size_t limit) : limit(limit) {}
-    bool operator() (const char * a, const char * b) {
-      return std::strncmp(a, b, limit) < 0;
+    LessStrLimited(const char * str, size_t limit) : str(str), limit(limit) {}
+    bool operator() (const char * a, const size_t b) {
+      return std::strncmp(a, str+b, limit) < 0;
+    }
+    bool operator() (const size_t a, const char * b) {
+      return std::strncmp(str+a, b, limit) < 0;
     }
   };
 public:
-  SuffixArray(const char * reference, const size_t ref_length)
-    : reference(reference), ref_length(ref_length), suffix_array(ref_length) {
+  SuffixArray(const char * ref_str, const size_t ref_length)
+    : ref_str(ref_str), ref_length(ref_length), suffix_array(ref_length) {
     for(size_t i=0; i < ref_length; ++i)
     {
-      suffix_array[i] = reference+i;
+      suffix_array[i] = i;
     }
-    std::sort(suffix_array.begin(), suffix_array.end(), LessStr());
+    std::sort(suffix_array.begin(), suffix_array.end(), LessStr(ref_str));
 #if 0
     for(size_t i=0; i < ref_length; ++i)
     {
@@ -46,12 +52,12 @@ public:
 #endif
   }
   answer findall_or_max(const char * query, const size_t max_hits) {
-    typedef std::vector<const char *>::iterator itr_t;
+    typedef std::vector<size_t>::iterator itr_t;
     const size_t l = std::strlen(query);
     itr_t lb = std::lower_bound(suffix_array.begin(),
-                                suffix_array.end()  , query, LessStrLimited(l));
+                                suffix_array.end()  , query, LessStrLimited(ref_str, l));
     itr_t ub = std::upper_bound(lb,
-                                suffix_array.end()  , query, LessStrLimited(l));
+                                suffix_array.end()  , query, LessStrLimited(ref_str, l));
     if ( !(lb < ub) )
     {
       return {0, nullptr};
@@ -65,7 +71,7 @@ public:
       itr_t it;
       for(it = lb, i = 0; it != ub; ++it, ++i)
       {
-        pos[i] = *it - $.reference;
+        pos[i] = *it;
       }
       std::sort(pos, pos + ans_size);
     }
