cd527a8af168ae1e39b46700731f3a9c6900f8ce
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2011-08-08 17:07:50 +0900

    Added large random test dataset.

--- test.cpp
+++ test.cpp
@@ -1,13 +1,37 @@
+#include <random>
+#include <algorithm>
+#include <functional>
+#include <boost/progress.hpp>
 #include "mysearch.h"
 
-void test(const char * str)
+void test(const char * str, bool print=false)
 {
-  printf("==test %s==\n", str);
+  if(print) printf("==test %s==\n", str);
+  boost::progress_timer t;
   SuffixArray SA(str, std::strlen(str));
 }
 
 int main()
 {
   //test("mississippi");
-  test("ababababcabcdabcdeabcdefabababacab");
+  test("ababababcabcdabcdeabcdefabababacab", true);
+  std::string buf;
+  const size_t SIZE = 10000000;
+  buf.resize(SIZE);
+
+  std::random_device rnddev;
+  std::vector< std::uint_least32_t> v(10);
+  std::generate(v.begin(), v.end(), std::ref(rnddev));
+  std::seed_seq seed(v.begin(), v.end());
+
+  std::mt19937 engine(seed);
+  std::uniform_int_distribution<int> distribution('a', 'z');
+  std::function<int()> rnd = [&]{ return distribution(engine); };
+
+  for(size_t i=0; i < SIZE; ++i)
+  {
+    if(i&1) buf[i] = 'a';
+    else buf[i] = rnd();
+  }
+  test(buf.c_str());
 }
