f712829e61d649e11ea82101f1ea965e66a2ebed
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2011-08-22 00:36:00 +0900

    Add shell script for final examination.

---
+++ final.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+./mysearch.O3 $1 100
+./mysearch.O3 $1 101
+./mysearch.O3 $1 102
+./mysearch.O3 $1 103
+./mysearch.O3 $1 110
+./mysearch.O3 $1 111
+./mysearch.O3 $1 112
+./mysearch.O3 $1 113
